64ce966484098f1a5796bafeb1de7676455d9224
Koutaroutarou <kw_extaro24karats@ezweb.ne.jp>
2019-06-03 05:15:14 +0900

    tableView作成

---
+++ tableview/RansuuTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RansuuTableViewController.swift
+//  tableview
+//
+//  Created by 渡辺航太郎 on 2019/06/03.
+//  Copyright © 2019 litech. All rights reserved.
+//
+
+import UIKit
+
+class RansuuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+  
+}
